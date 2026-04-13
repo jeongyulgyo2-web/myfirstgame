8f8c283230e71c66b8ed4f6e6aeb48533d884c40
bakabu <jeongyulgyo2@gmail.com>
2026-04-13 21:01:42 +0900

    움직임

---
+++ My project (1)/Assets/PrayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
+
+public class PrayerController : MonoBehaviour
+{
+    private Vector2 moveInput;
+    public float moveSpeed = 7f;
+    public float jumpforce = 7f;
+    private Rigidbody2D rb;
+    void Start()
+    {
+      rb= GetComponent<Rigidbody2D>();
+    }
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpforce);
+        }
+    }
+    void Update()
+    {
+        if(moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if(moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
+    }
+}
